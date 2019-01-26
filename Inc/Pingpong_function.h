ddc85fb9984bc4ea6be99e47127062b44773c3f8
JunyuC032211 <42887535+JunyuC032211@users.noreply.github.com>
2019-01-26 12:34:06 +0100

    Pingpongtest1

---
+++ Inc/Pingpong_function.h
@@ -0,0 +1,22 @@
+
+
+#ifndef __Pingpong_function_H
+#define __Pingpong_function_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f3xx_hal.h"
+#include "main.h"
+
+void Led_on(uint8_t Lednr);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __Pingpong_function_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ Inc/clockinit.h
@@ -0,0 +1,22 @@
+
+
+#ifndef __clockinit_H
+#define __clockinit_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f3xx_hal.h"
+#include "main.h"
+
+void SystemClock_Config(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
