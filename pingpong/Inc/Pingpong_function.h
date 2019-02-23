38d8003482953548e94d7a362bbbaa8e22723ac1
JunyuC032211 <42887535+JunyuC032211@users.noreply.github.com>
2019-02-23 14:39:15 +0100

    refactor pre-labb pingpong and create new cude for lab

---
+++ pingpong/Inc/Pingpong_function.h
@@ -0,0 +1,38 @@
+
+
+#ifndef __Pingpong_function_H
+#define __Pingpong_function_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f3xx_hal.h"
+#include "main.h"
+#include "stdbool.h"
+
+void Led_on(uint8_t Lednr);
+
+void Show_points(uint8_t L_points, uint8_t R_points);
+
+/**
+@brief L_hit, check if L button is pressed
+@param void
+@return bool, true if L button pushed, false otherwise
+*/
+bool L_hit(void);
+/**
+@brief R_hit, check if R button is pressed
+@param void
+@return bool, true if R button pushed, false otherwise
+*/
+bool R_hit(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __Pingpong_function_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
